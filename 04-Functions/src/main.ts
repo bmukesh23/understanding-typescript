3611c71ec5d493e0de9746aa44494c156dd3a46a
Mukesh <billamukesh1234@gmail.com>
2024-03-19 21:20:42 +0530

    finised functions

--- 04-Functions/src/main.ts
+++ 04-Functions/src/main.ts
@@ -1,5 +1,17 @@
-interface Guitarist {
-    name?: string,
-    active: boolean,
-    albums: (string | number)[]
+// literal types
+let myName: 'Dave'
+
+let userName: 'Dave' | 'John' | 'Amy'
+userName = 'Amy'
+
+// functions
+const add = (a: number, b: number): number => {
+    return a + b;
 }
+
+const logMsg = (message: any): void => {
+    console.log(message);
+}
+
+logMsg('Hello!');
+logMsg(add(2,3));
